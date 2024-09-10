14af952d7cb6fd32c738e842f56d39aecb717bfa
dennis626 <133036850+dennis626@users.noreply.github.com>
2024-09-11 00:12:47 +0800

    Airbnb_v12_Streamlit_v2.py

--- Airbnb_v12_Streamlit_v2.py
+++ Airbnb_v12_Streamlit_v2.py
@@ -182,11 +182,11 @@ def display_visual_analysis():
     counts_order = counts.index
     fig, ax = plt.subplots(figsize=(10, 7))
     sns.countplot(x=user_data['country_destination'], ax=ax)
-    ax.xlabel('Destination Country')
-    ax.ylabel('Count')
-    ax.title('Destination Country Distribution')
+    plt.xlabel('Destination Country')
+    plt.ylabel('Count')
+    plt.title('Destination Country Distribution')
     for i in range(counts.shape[0]):
-        ax.text(i, counts[i]+1000, f"{counts[i]/user_data.shape[0]*100:0.2f}%", ha='center', fontsize=10)
+        plt.text(i, counts[i]+1000, f"{counts[i]/user_data.shape[0]*100:0.2f}%", ha='center', fontsize=10)
     sns.despine()
     st.pyplot(fig)
 
