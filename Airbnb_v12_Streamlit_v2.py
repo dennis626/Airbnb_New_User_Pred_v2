14ae1686071e7fb179eeb0d1700c26b24b35d896
dennis626 <133036850+dennis626@users.noreply.github.com>
2024-09-10 23:04:09 +0800

    Airbnb_v12_Streamlit_v2.py

--- Airbnb_v12_Streamlit_v2.py
+++ Airbnb_v12_Streamlit_v2.py
@@ -89,7 +89,8 @@ def display_visual_analysis():
     # Signup Method Distribution
     st.write("#### Signup Method Distribution")
     fig, ax = plt.subplots(figsize=(9, 6))
-    sns.countplot(x=user_data['signup_method'], ax=ax)
+    counts = user_data['signup_method'].fillna('NaN').value_counts(dropna=False)
+    sns.countplot(x=user_data['signup_method'].fillna('NaN'), order=counts.index, ax=ax)
     for i in range(len(counts)):
         ax.text(i, counts[i] + 1200, f"{counts[i]/user_data.shape[0]*100:0.2f}%", ha='center', fontsize=10)
     st.pyplot(fig)
